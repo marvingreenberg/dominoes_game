4417758dddd14b117187d8d959d6e5dfc4795834
Marvin Greenberg <marvin.greenberg@gmail.com>
2019-08-10 13:26:41 -0400

    changes to support css, changes to support rotations

--- mkdom.py
+++ mkdom.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import unicode_literals, print_function
 import argparse
 import os
 import sys
@@ -12,6 +13,9 @@ RECT = 'rect'
 
 DWIDTH = 96
 DHEIGHT = 192
+DBORDER = 5
+DPIPRADIUS = 7
+
 ZERO = []
 ONE = [(48, 48)]
 TWO = [(78, 16), (16, 78)]
@@ -23,7 +27,6 @@ SIX = [(78, 16), (78, 47), (78, 78), (16, 16), (16, 47), (16, 78)]
 PIP_LAYOUT = [ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX]
 TOP = 'matrix(1, 0, 0, 1, 5, 5)'
 BOTTOM = 'matrix(1, 0, 0, 1, 5, 101)'
-
 
 def indent(elem, level=0):
     i = "\n" + level*"  "
@@ -45,9 +48,9 @@ def indent(elem, level=0):
 
     return elem
 
+SIZING={}
 
-SIZING = {
-    'enable-background': 'new 0.0 0.0 30.0 60.0',
+_SIZING = {
     'height': '60.0mm',
     'width': '30.0mm',
     'x': '0.0mm',
@@ -56,86 +59,126 @@ SIZING = {
     'viewBox': 'new 0.0 0.0 96.0 192.0'
 }
 
+RECTSTYLE = (
+     'stroke: rgb({fg}); fill: rgb({bg}); stroke-width: 2px; stroke-linejoin: round; stroke-linecap: round; '
+    )
+LINESTYLE= 'stroke: rgb({fg}); stroke-width: 1px; '
+PIPSTYLE = 'stroke: rgb({fg}); fill: rgb({fg}); '
+
+PIPCLASS = 'pip'
+LINECLASS = 'line'
+RECTCLASS = 'rect'
+
+def styles(css_styles, cls, explicitstyle, *args, **kwargs):
+    if css_styles:
+        return {'class': cls}
+    else:
+        return {'style': explicitstyle.format(*args,**kwargs)}
+
+def css_def(*args,**kwargs):
+    linestyle=LINESTYLE.format(*args, **kwargs)
+    rectstyle=RECTSTYLE.format(*args, **kwargs)
+    pipstyle=PIPSTYLE.format(*args, **kwargs)
+
+    return ('.{rect} {{ {rectstyle} }}\n'
+            '.{line} {{ {linestyle} }}\n'
+            '.{pip}  {{ {pipstyle} }}\n').format(
+                line=LINECLASS, rect=RECTCLASS, pip=PIPCLASS,
+                linestyle=linestyle, pipstyle=pipstyle, rectstyle=rectstyle )
 
 class DominoSVG(object):
-    def __init__(self, topnum, bottomnum, fg='0,0,0', bg='255,255,255'):
-        assert topnum in range(7) and bottomnum in range(7)
-        self.topnum = topnum
-        self.bottomnum = bottomnum
-        self.init_svg(fg, bg)
+    def __init__(self, num1, num2, fg='0,0,0', bg='255,255,255', matrix='', css_styles=False):
+        assert num1 in range(7) and num2 in range(7)
+        # always order tuples lower, higher
+        self.dtuple = (num1,num2) if num1 < num2 else (num2,num1)
+        self.init_svg(fg, bg, matrix, css_styles)
 
-    def init_svg(self, fg, bg):
+    def init_svg(self, fg, bg, matrix, css_styles):
         self.svg = ET.Element('svg',
                               attrib=SIZING,
                               xmlns='http://www.w3.org/2000/svg')
         self.svg.text = '\n'
-        style = 'stroke: rgb({fg}); fill: rgb({bg}); '.format(fg=fg, bg=bg)
-        style += 'stroke-width: 2px; '
-        style += 'stroke-linejoin: round; stroke-linecap: round; '
-        ET.SubElement(self.svg, RECT, x='5', y='5',
-                      width=str(DWIDTH), height=str(DHEIGHT),
-                      style=style)
-        style = 'stroke: rgb({fg}); stroke-width: 1px;'.format(fg=fg)
-        ET.SubElement(self.svg, LINE, x1='11', y1='101', x2='95', y2='101',
-                      style=style)
-        self.tile_grp(TOP, self.topnum, fg, bg)
-        self.tile_grp(BOTTOM, self.bottomnum, fg, bg)
 
-    def tile_grp(self, transform, number, fg='0,0,0', bg='255,255,255'):
+        attrib = {'transform':'matrix({0})'.format(matrix)} if matrix else {}
+        outer_grp = ET.SubElement(self.svg, GROUP, attrib=attrib)
+
+        styleattrib = styles(css_styles, RECTCLASS, RECTSTYLE, fg=fg, bg=bg)
+        ET.SubElement(outer_grp, RECT, x='5', y='5',
+                      width=str(DWIDTH), height=str(DHEIGHT),
+                      attrib=styleattrib)
+
+        styleattrib = styles(css_styles, LINECLASS, LINESTYLE, fg=fg)
+        ET.SubElement(outer_grp, LINE, x1='11', y1='101', x2='95', y2='101',
+                      attrib=styleattrib)
+
+        styleattrib = styles(css_styles, PIPCLASS, PIPSTYLE, fg=fg)
+        self.tile_grp(outer_grp, TOP, self.dtuple[0], attrib=styleattrib)
+        self.tile_grp(outer_grp, BOTTOM, self.dtuple[1], attrib=styleattrib)
+
+    def tile_grp(self, grp, transform, number, attrib):
         pips = PIP_LAYOUT[number]
         if pips:
-            grp = ET.SubElement(self.svg, GROUP, transform=transform)
-            style = 'stroke: rgb({fg}); fill: rgb({fg})'.format(fg=fg)
+            tilegrp = ET.SubElement(grp, GROUP, transform=transform)
             for pip in pips:
-                ET.SubElement(grp, CIRCLE, r='7', cx=str(pip[0]), cy=str(pip[1]),
-                              style=style)
-
+                ET.SubElement(tilegrp, CIRCLE, r=str(DPIPRADIUS),
+                              cx=str(pip[0]), cy=str(pip[1]), attrib=attrib)
 
 def main():
     ap = argparse.ArgumentParser()
-    ap.add_argument('--html', action='store_true')
-    ap.add_argument('--dir', action='store', default=None)
-    ap.add_argument('--foreground', '-f', action='store', default='(0,0,0)')
-    ap.add_argument('--background', '-b', action='store',
-                    default='(255,255,255)')
+    ap.add_argument('--python', action='store_true',
+                    help='Generate as a dict in a python module, ignore foreground, background')
+    ap.add_argument('--dir', action='store', default='.')
+    ap.add_argument('--foreground', '-f', action='store', default='0,0,0')
+    ap.add_argument('--background', '-b', action='store', default='255,255,255')
+    ap.add_argument('--matrix', '-m', action='store', default='',
+                    help='Elements of a 2d transformation, e.g "1,0,0,1,0,0"')
     args = ap.parse_args()
 
-    dominoes = [DominoSVG(i, j, fg=args.foreground, bg=args.background)
-                for i in range(7) for j in range(i, 7)]
-    if args.dir and not os.path.exists(args.dir):
+    fg = '{fg}' if args.python else args.foreground
+    bg = '{bg}' if args.python else args.background
+    matrix = '{matrix}' if args.python else args.matrix
+    dominoes = [DominoSVG(i, j, fg=fg, bg=bg, matrix=matrix, css_styles=True)
+                 for i in range(7) for j in range(i, 7)]
+
+    if not os.path.exists(args.dir):
         os.makedirs(args.dir)
-    if args.html:
-        mkhtml(dominoes, outdir=args.dir)
+
+    if args.python:
+        mkpython(dominoes, css_styles=True, outdir=args.dir)
     else:
+        with open(os.path.join(args.dir,'dominoes.css'), 'w') as f:
+            f.write(css_def(fg=fg, bg=bg))
         mkfiles(dominoes, outdir=args.dir)
 
 
-def mkfiles(dominoes, outdir=None):
+def mkfiles(dominoes, outdir='.'):
+    '''Write the dominoes as individual SVG files'''
+
     for d in dominoes:
-        ofile = os.path.join(outdir if outdir else '.',
-                             '{}_{}.svg'.format(d.topnum, d.bottomnum))
+        ofile = os.path.join(outdir, '{}_{}.svg'.format(*d.dtuple))
         with open(ofile, 'w') as f:
             t = ET.ElementTree(element=indent(d.svg))
             t.write(f, encoding='utf-8', xml_declaration=True)
 
 
-def mkhtml(dominoes, outdir=None):
-    ofile = os.path.join(outdir if outdir else '.',
-                         'dominos.html')
-
-    html = ET.Element('html', lang='en')
-    body = ET.SubElement(html, 'body')
-    for n in 7, 6, 5, 4, 3, 2, 1:
-        ul = ET.SubElement(body, 'ul', attrib={'class': 'table'})
-        row = dominoes[:n]
-        del dominoes[:n]
-
-        li = ET.SubElement(ul, 'li', attrib={'class': 'row'})
-        li.extend([d.svg for d in row])
-
+def mkpython(dominoes, module='dominoes', outdir='.', css_styles=True):
+    '''Write the dominoes as constants in a module named dominoes'''
+    ofile = os.path.join(outdir, '{}.py'.format(module))
     with open(ofile, 'w') as f:
-        t = ET.ElementTree(element=indent(html))
-        t.write(f, encoding='utf-8', xml_declaration=True)
+        f.write("DWIDTH = {}\n".format(DWIDTH))
+        f.write("DHEIGHT = {}\n".format(DHEIGHT))
+        # write some rotation matrixes
+        f.write("M2d_IDENTITY = '1, 0, 0, 1, 0, 0'\n")
+        f.write("M2d_ROTLEFT = '0, -1, 1, 0, 0, {}'\n".format(DWIDTH+2*DBORDER))
+        f.write("M2d_ROTRIGHT = '0, 1, -1, 0, 0, {}'\n".format(DHEIGHT+2*DBORDER ))
+        f.write("SVG_DICT = dict( (\n")
+        for d in dominoes:
+            f.write("    ({}, '''{}'''),\n".format(
+                d.dtuple, ET.tostring(d.svg).decode('utf-8')))
+        f.write("    )\n")
+        if css_styles:
+            f.write("DOMINO_CSS = '''{}'''\n".format(
+                css_def(fg='0,0,0',bg='255,255,255')))
 
 
 if __name__ == '__main__':
